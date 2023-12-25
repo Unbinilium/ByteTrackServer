3493d11e008deacc22f95170d236f382bdc91330
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2023-12-25 20:43:00 +0800

    fix: type checking

--- bytetrack/sscma_utilitiy.py
+++ bytetrack/sscma_utilitiy.py
@@ -18,32 +18,26 @@ def parse_bytes_to_json(request: bytes) -> dict:
         logging.warning('Failed to parse request to json to dict')
         raise ValueError from exc
 
-def xywh_to_xyxy(xywh: list) -> list:
-    xyxy = xywh[:4]
-    xyxy[2] = xyxy[0] + xyxy[2]
-    xyxy[3] = xyxy[1] + xyxy[3]
+def xywh_to_xyxy(xywh: np.ndarray) -> np.ndarray:
+    xyxy = np.asarray(xywh[:4])
+    xyxy[2:4] = xyxy[2:4] + xyxy[0:2]
     return xyxy
 
-def xyxy_to_xywh(xyxy: list) -> list:
-    xywh = xyxy[:4]
-    xywh[2] = xywh[2] - xywh[0]
-    xywh[3] = xywh[3] - xywh[1]
+def xyxy_to_xywh(xyxy: np.ndarray) -> np.ndarray:
+    xywh = np.asarray(xyxy[:4])
+    xywh[2:4] = xywh[2:4] - xywh[0:2]
     return xywh
 
-def cxcywh_to_xyxy(cxcywh: list) -> list:
-    xyxy = cxcywh[:4]
-    xyxy[0] = xyxy[0] - xyxy[2] / 2
-    xyxy[1] = xyxy[1] - xyxy[3] / 2
-    xyxy[2] = xyxy[0] + xyxy[2]
-    xyxy[3] = xyxy[1] + xyxy[3]
+def cxcywh_to_xyxy(cxcywh: np.ndarray) -> np.ndarray:
+    xyxy = np.asarray(cxcywh[:4])
+    xyxy[0:2] = xyxy[0:2] - (xyxy[2:4] / 2.0)
+    xyxy[2:4] = xyxy[0:2] + xyxy[2:4]
     return xyxy
 
-def xyxy_to_cxcywh(xyxy: list) -> list:
-    cxcywh = xyxy[:4]
-    cxcywh[2] = cxcywh[2] - cxcywh[0]
-    cxcywh[3] = cxcywh[3] - cxcywh[1]
-    cxcywh[0] = cxcywh[0] + cxcywh[2] / 2
-    cxcywh[1] = cxcywh[1] + cxcywh[3] / 2
+def xyxy_to_cxcywh(xyxy: np.ndarray) -> np.ndarray:
+    cxcywh = np.asarray(xyxy[:4])
+    cxcywh[0:2] = cxcywh[0:2] + (cxcywh[2:4] / 2.0)
+    cxcywh[2:4] = cxcywh[2:4] - cxcywh[0:2]
     return cxcywh
 
 @classmethod
@@ -54,34 +48,38 @@ def from_sscma_detection(cls: Detections, detection: dict) -> Detections:
     boxes = detection['boxes'] # [x,y,w,h,conf,class_id]
 
     try:
-        boxes = np.array(boxes)
+        boxes = np.asarray(boxes)
+        if len(boxes.shape) > 2:
+            raise ValueError
+        if len(boxes.shape) == 2 and boxes.shape[1] != 6:
+                raise ValueError
     except Exception as exc:
         logging.warning('Failed to convert boxes to numpy array')
         raise ValueError from exc
 
     CONFIDENCE = 4
     CLASS_ID = 5
-    xyxys = []
-    confidences = []
-    class_ids = []
-    for box in boxes:
-        xyxys.append(cxcywh_to_xyxy(box))
-        confidences.append(box[CONFIDENCE] / 100.0)
-        class_ids.append(box[CLASS_ID])
+    LEN = len(boxes)
+    xyxys = np.empty((LEN, 4))
+    confidences = np.empty((LEN))
+    class_ids = np.empty((LEN), dtype=int)
+    for i, box in enumerate(boxes):
+        xyxys[i] = xywh_to_xyxy(box)
+        confidences[i] = box[CONFIDENCE] / 100.0
+        class_ids[i] = box[CLASS_ID]
 
     return cls(
-        xyxy=np.asarray(xyxys),
-        confidence=np.asarray(confidences),
-        class_id=np.asarray(class_ids).astype(int)
-    )
+        xyxy=xyxys,
+        confidence=confidences,
+        class_id=class_ids)
 
 def detection_to_tracked_bboxs(detection: Detections) -> list:
-    xyxys = np.round(detection.xyxy).astype(int).tolist()
+    cxcywhs = np.round([xyxy_to_cxcywh(xyxy) for xyxy in detection.xyxy]).astype(int).tolist()
     confidences = np.round(detection.confidence * 100.0).astype(int).tolist()
     class_ids = detection.class_id.astype(int).tolist()
     tracker_ids = detection.tracker_id.tolist()
-    return [[*xyxy_to_cxcywh(xyxy), conf, class_id, tracker_id]
-             for xyxy, conf, class_id, tracker_id in zip(xyxys, confidences, class_ids, tracker_ids)]
+    return [[*cxcywh, conf, class_id, tracker_id]
+             for cxcywh, conf, class_id, tracker_id in zip(cxcywhs, confidences, class_ids, tracker_ids)]
 
 def image_from_base64(base64_image: str) -> np.ndarray:
     try:
